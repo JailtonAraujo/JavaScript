2069d2ddb2eb7fe11798f26c675f089ac02f2fdc
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2022-01-22 17:56:21 -0300

    COndição swith jS

---
+++ CursoJS/Aulo-11/ex016.js
@@ -0,0 +1,32 @@
+var agora = new Date();
+
+var DiaSemana = agora.getDay();
+
+console.log(DiaSemana);
+
+switch(DiaSemana){
+    case 0:
+        console.log('Hoje é Domingo!');
+        break;
+    case 1:
+        console.log('Hoje é Segunda!');
+        break;
+    case 2:
+        console.log('Hoje é Terça!');
+        break; 
+    case 3:
+        console.log('Hoje é Quarta!');
+        break;  
+    case 4:
+        console.log('Hoje é Quinta!');
+        break; 
+    case 5:
+        console.log('Hoje é Sexta!');
+        break;
+    case 6:
+        console.log('Hoje é Sabado!');
+        break; 
+    default:
+        console.log('Nenhum di valido foi dectado!');
+        break;
+}
