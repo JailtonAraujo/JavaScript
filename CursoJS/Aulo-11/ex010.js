5cb0dd3555ccd9aebecda65355747b0646a7f8f3
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2022-01-22 15:34:54 -0300

    condições em JS

---
+++ CursoJS/Aulo-11/ex010.js
@@ -0,0 +1,8 @@
+var vel = 60.2;
+console.log(`A veocidade do seu carro é ${vel}Km/h `);
+
+console.log(`Dirige sempre com cuidado`);
+
+if (vel > 60){ /* Condição seimples*/
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+}
