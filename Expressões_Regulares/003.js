14ed98aaa8fc38306286dad348b45e8b3de195d9
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2022-03-06 00:41:54 -0300

    Update 003.js

--- Expressões_Regulares/003.js
+++ Expressões_Regulares/003.js
@@ -1,1 +1,13 @@
 const {texto} = require('./base');
+
+//Quantificadores.
+
+// * (opcionais) 0 ou n
+// + (opcionais) 1 ou n
+// ? (opcionais) 0 ou 1
+
+console.log(texto);
+
+const regExp1 = /jo+ão+/gi
+
+console.log(texto.match(regExp1));
